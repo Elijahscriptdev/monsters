6a2aced9b61d828c6eb6b515624a300b9b7eb6e4
Elijahscriptdev <elijahobominuru@gmail.com>
2020-08-15 02:37:39 +0100

    Updates

---
+++ precache-manifest.309f61330ef302df1e683544e156cb13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9164dfa247633dad3393f2e452c5d9f7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2e317e21450e40e5bf7e",
+    "url": "/monsters-rolodex/static/css/main.a6a997a8.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e317e21450e40e5bf7e",
+    "url": "/monsters-rolodex/static/js/main.0d847b08.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
